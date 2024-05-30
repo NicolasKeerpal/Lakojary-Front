46eadc67b0edbb675f6dc66c78675bc42cff20c1
Yann <yann.sady.pro@gmail.com>
2024-05-30 09:26:04 +0200

    🚧 get profile (in console.log)

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { getCustomer } from '../services/CustomerService';
+import { getEmployee } from '../services/EmployeeService';
+import { jwtDecode } from 'jwt-decode';
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          firstname: '',
+          lastname: '',
+          mail: '',
+          role: null
+        };
         this.logout = this.logout.bind(this);
       }
+
+    async componentDidMount() {
+      const token = localStorage.getItem('token');
+      let decoded;
+      if (token) {
+        try {
+          decoded = jwtDecode(token);
+          this.setState({ role: decoded.role });
+        } catch (error) {
+          alert("Une erreur est survenue");
+          this.props.navigate('/home');
+        }
+      }
+  
+      try {
+          let userData, firstname, lastname, mail;
+          if (decoded.role=='client') {
+            userData = await getCustomer(decoded.id);
+            firstname = userData.data.customerId.firstname;
+            lastname = userData.data.customerId.lastname;
+            mail = userData.data.customerId.mail;
+          } else {
+            userData = await getEmployee(decoded.id);
+            firstname = userData.data.employeeId.firstname;
+            lastname = userData.data.employeeId.lastname;
+            mail = userData.data.employeeId.mail;
+          }
+          
+          if (userData.success) {
+              this.setState({ 
+                  firstname,
+                  lastname,
+                  mail
+              });
+          }
+      } catch(error) {
+          alert("Une erreur est survenue");
+          this.props.navigate('/home');
+      }
+    }
 
     logout = () => {
       localStorage.removeItem('token');
@@ -13,6 +62,7 @@ class Profile extends React.Component {
     }
   
     render() {
+      console.log(this.state);
       return (
         <div>
           <h1>Profil</h1>
