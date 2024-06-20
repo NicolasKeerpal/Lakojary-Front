4316e83d424d952fa0b5163a3f8def9e38ef9346
Yann <yann.sady.pro@gmail.com>
2024-06-20 15:21:18 +0200

    ✨ add update and delete food

--- src/services/FoodService.js
+++ src/services/FoodService.js
@@ -59,3 +59,63 @@ export const addFood = async (name, price, description, stock, image, compositio
       return { success: false, message: "Une erreur est survenue" };
   }
 };
+
+export const delFood = async (id) => {
+  const token = localStorage.getItem('token');
+  try {
+    if (token) {
+      const response = await axios.delete(`${url}/foods/${id}`, {
+        headers: {
+          Authorization: token
+        }
+      });
+
+      return response; 
+    }
+    return { success: false, message: "Une erreur est survenue" };
+  } catch (error) {
+    if (error.response && error.response.data) {
+        return error.response.data;
+      }
+      return { success: false, message: "Une erreur est survenue" };
+  }
+};
+
+export const putFood = async (id, name, price, description, addStock, image, composition) => {
+  const token = localStorage.getItem('token');
+  try {
+    if (token) {
+      const formData = new FormData();
+      formData.append('name', name);
+      formData.append('description', description);
+      formData.append('price', price);
+      formData.append('addStock', addStock);
+      formData.append('image', image);
+      const response = await axios.put(`${url}/foods/${id}`, formData, {
+        headers: {
+          'Authorization': token,
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      const idsComposition = composition.map(ingredient => ingredient.id);
+      const compositionData = {
+        composition: idsComposition
+      };
+
+      await axios.put(`${url}/compositions/food/${id}`, compositionData, {
+        headers: {
+          Authorization: token
+        }
+      });
+      
+      return response; 
+    }
+    return { success: false, message: "Une erreur est survenue" };
+  } catch (error) {
+    if (error.response && error.response.data) {
+        return error.response.data;
+      }
+      return { success: false, message: "Une erreur est survenue" };
+  }
+};
