10c40c794ec1e3e842ec96778dc4d567015c60ed
NicolasKeerpal <nicolaskeerpal@gmail.com>
2024-07-13 15:36:42 +0200

    Update Navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode'; // Corrected import
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -30,70 +30,73 @@ class Navbar extends React.Component {
         case 'admin':
           return (
             <>
-              <li><Link to="/">Accueil</Link></li>
-              <li><Link to="/nos-produits">Nos produits</Link></li>
-              <li><Link to="/qui-sommes-nous">Qui sommes nous ?</Link></li>
-              <li><Link to="/ingredients">Ingrédients</Link></li>
-              <li>
-                <span>Utilisateurs</span>
-                <ul>
-                  <li><Link to="/clients">Clients</Link></li>
-                  <li><Link to="/employes">Employés</Link></li>
-                </ul>
-              </li>
-              <li><Link to="/remplir-bdd">Remplir BDD</Link></li>
-              <li><Link to="/profil">Profil</Link></li>
-            </>
+            <li><Link to="/" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Accueil</Link></li>
+            <li><Link to="/nos-produits" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Nos produits</Link></li>
+            <li><Link to="/qui-sommes-nous" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Qui sommes nous ?</Link></li>
+            <li><Link to="/ingredients" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Ingrédients</Link></li>
+            <li>
+              <span className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Utilisateurs</span>
+              <ul>
+                <li><Link to="/clients" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Clients</Link></li>
+                <li><Link to="/employes" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Employés</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/remplir-bdd" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Remplir BDD</Link></li>
+            <li><Link to="/profil" className="bg-custom-primary_color text-white hover:text-white hover:bg-custom-hover_effect text-xl font-bold rounded-[5rem] px-[2rem] py-[0.2rem]">Profil</Link></li>
+          </>
           );
         case 'livreur':
           return (
             <>
-              <li><Link to="/">Accueil</Link></li>
-              <li><Link to="/nos-produits">Nos produits</Link></li>
-              <li><Link to="/qui-sommes-nous">Qui sommes nous ?</Link></li>
-              <li><Link to="/livraisons">Livraisons</Link></li>
-              <li><Link to="/profil">Profil</Link></li>
+              <li><Link to="/" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Accueil</Link></li>
+              <li><Link to="/nos-produits" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Nos produits</Link></li>
+              <li><Link to="/qui-sommes-nous" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Qui sommes nous ?</Link></li>
+              <li><Link to="/livraisons" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Livraisons</Link></li>
+              <li><Link to="/profil" className="bg-custom-primary_color text-white hover:text-white hover:bg-custom-hover_effect text-xl font-bold rounded-[5rem] px-[2rem] py-[0.2rem]">Profil</Link></li>
             </>
           );
         case 'client':
           return (
             <>
-              <li><Link to="/">Accueil</Link></li>
-              <li><Link to="/nos-produits">Nos produits</Link></li>
-              <li><Link to="/qui-sommes-nous">Qui sommes nous ?</Link></li>
-              <li><Link to="/commandes">Commandes</Link></li>
-              <li><Link to="/panier">Panier</Link></li>
-              <li><Link to="/profil">Profil</Link></li>
+              <li><Link to="/" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Accueil</Link></li>
+              <li><Link to="/nos-produits" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Nos produits</Link></li>
+              <li><Link to="/qui-sommes-nous" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Qui sommes nous ?</Link></li>
+              <li><Link to="/commandes" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Commandes</Link></li>
+              <li><Link to="/panier" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Panier</Link></li>
+              <li><Link to="/profil" className="bg-custom-primary_color text-white hover:text-white hover:bg-custom-hover_effect text-xl font-bold rounded-[5rem] px-[2rem] py-[0.2rem]">Profil</Link></li>
             </>
           );
         case 'boulanger':
           return (
             <>
-              <li><Link to="/">Accueil</Link></li>
-              <li><Link to="/nos-produits">Nos produits</Link></li>
-              <li><Link to="/ingredients">Ingrédients</Link></li>
-              <li><Link to="/qui-sommes-nous">Qui sommes nous ?</Link></li>
-              <li><Link to="/profil">Profil</Link></li>
+              <li><Link to="/" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Accueil</Link></li>
+              <li><Link to="/nos-produits" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Nos produits</Link></li>
+              <li><Link to="/ingredients" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Ingrédients</Link></li>
+              <li><Link to="/qui-sommes-nous" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Qui sommes nous ?</Link></li>
+              <li><Link to="/profil" className="bg-custom-primary_color text-white hover:text-white hover:bg-custom-hover_effect text-xl font-bold rounded-[5rem] px-[2rem] py-[0.2rem]">Profil</Link></li>
             </>
           );
         default:
           return (
             <>
-              <li className="nav-item"><Link to="/">Accueil</Link></li>
-              <li className="nav-item"><Link to="/nos-produits">Nos produits</Link></li>
-              <li className="nav-item"><Link to="/qui-sommes-nous">Qui sommes nous ?</Link></li>
-              <li className="nav-item"><Link to="/connexion">Connexion</Link></li>
+              <li><Link to="/" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Accueil</Link></li>
+              <li><Link to="/nos-produits" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Nos produits</Link></li>
+              <li><Link to="/qui-sommes-nous" className="relative text-white hover:text-custom-hover_effect text-xl font-bold">Qui sommes nous ?</Link></li>
+              <li><Link to="/connexion" className="bg-custom-primary_color text-white hover:text-white hover:bg-custom-hover_effect text-xl font-bold rounded-[5rem] px-[2rem] py-[0.2rem]">Connexion</Link></li>
             </>
           );
       }
     };
 
     return (
-      <nav className='nav_container'>
-        <ul>
-          {getNavbar()}
-        </ul>
-      </nav>
+      <header className="bg-custom-secondary_color sticky top-0 z-10">
+        <nav className="container mx-auto px-4 py-2 flex items-center justify-between">
+          <img className="w-[4rem] h-[3.5rem] p-0" src="/path/to/your/logo.png" alt="Logo" />
+          <ul className="flex space-x-4">
+            {getNavbar()}
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
